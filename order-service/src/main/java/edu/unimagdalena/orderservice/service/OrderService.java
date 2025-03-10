7b4d83c977c167b9fb1e19a407fa1d1d5d214404
Orion <wilman3d@hotmail.com>
2025-03-10 09:09:20 -0500

    feat: add order management endpoints and service implementation

--- order-service/src/main/java/edu/unimagdalena/orderservice/service/OrderService.java
+++ order-service/src/main/java/edu/unimagdalena/orderservice/service/OrderService.java
@@ -1,4 +1,53 @@
 package edu.unimagdalena.orderservice.service;
 
+import edu.unimagdalena.orderservice.entity.Order;
+import edu.unimagdalena.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public Flux<Order> getAllOrders() {
+        return Flux.defer(() -> Flux.fromIterable(orderRepository.findAll()))
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+    public Mono<Order> getOrderById(String id){
+        return Mono.defer(() -> Mono.justOrEmpty(orderRepository.findById(id)))
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    public Mono<Order> createOrder(Order order){
+        order.setId(UUID.randomUUID().toString());
+        return Mono.defer(() -> Mono.just(orderRepository.save(order)))
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    public Mono<Order> updateOrder(String id, Order order){
+        return Mono.defer(() -> {
+            if(orderRepository.existsById(id)){
+                order.setId(id);
+                return Mono.just(orderRepository.save(order));
+            }
+            return Mono.empty();
+        }).subscribeOn(Schedulers.boundedElastic());
+    }
+
+    public Mono<Void> deleteOrder(String id){
+        return Mono.defer(() -> {
+            if(orderRepository.existsById(id)){
+                orderRepository.deleteById(id);
+                return Mono.empty();
+            }
+            return Mono.error(new NoSuchElementException("Order not fount"));
+        }).subscribeOn(Schedulers.boundedElastic()).then();
+    }
 }
